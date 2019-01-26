cca39c4697aa221a5b7137c181fdeb2280355175
Graithen <grey.jim@gmail.com>
2019-01-26 15:01:11 +0000

    File push
    Project file push.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5;
+    float acceleration = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        float posX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float posZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+         Vector3 move = new Vector3(transform.position.x + posX, transform.position.y, transform.position.z + posZ);
+        //move = move.normalized;
+        transform.position = move;
+    }
+}
